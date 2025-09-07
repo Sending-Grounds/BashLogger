2ef51410559d5259d6e8dba3dca177f270d8aa62
Sending_Grounds <32382199+Sending-Grounds@users.noreply.github.com>
2025-09-07 14:21:21 -0700

    feat(BashLogger): add wrapper functions for ease of use
    ## Overview
    ### Feature 
    We currently only allow "log <LOG_LEVEL> <LOG_MESSAGE>", but this is a different design pattern than most other languages like Python where you would `log_info`. We are adding some wrapper scripts to support this functionality and fixing a bug with handling args
    
    **R1**
    * Added wrappers/syntatic sugar for `log_info` , `log_debug`, etc...
    * Added arg check, so if user does not pass in log level we default to INFO
    
    ## Testing
    Manual testing along with ~20 Bats tests (which will add later)
    
    ```bash
    ...<TRUNCATED>...
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating base logging functionality works
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating can log at INFO log level
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating can log at WARN log level
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating can log at ERROR log level
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating can log at DEBUG log level
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating no log will be produced at DEBUG log level, if DEBUG is set to False
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating no log will be produced at DEBUG log level, if DEBUG is not set at all
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating no log will be produced at DEBUG log level, if DEBUG is set to empty string
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating no log will be produced at DEBUG log level, if DEBUG is set to a string other than "True" exactly
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating log fails if invalid log level is provided
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating log defaults to INFO if no log level is provided
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log_debug(), validating can log at DEBUG log level
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log_info(), validating can log at INFO log level
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log_warn(), validating can log at WARN log level
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log_error(), validating can log at ERROR log level
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating can log with variables
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating can log with variables and quotes
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating logs with correct timestamp
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating logs with correct timestamp and variables
     ✓ Library "/home/sending_grounds/lib/bash-logger.sh", function: log(), validating logs handle metacharacters correctly
    
    20 tests, 0 failures
    ```

--- lib/bash-logger.sh
+++ lib/bash-logger.sh
@@ -2,10 +2,20 @@
 
 # Bash Logger function takes log level and message as 
 function log() {
-  local log_level=${1:-INFO}
-  local message=$2
-  local timestamp=$(date +'%Y-%m-%d %H:%M:%S')
+  local log_level=""
+  local message=""
+  local timestamp=""
+  timestamp=$(date +'%Y-%m-%d %H:%M:%S')
 
+  if [[ $# -eq 1 ]]; then
+    printf "%s [DEBUG] No log level provided. Defaulting to INFO\n" "${timestamp}"
+    log_level="INFO"
+    message=${1} # Bash-ish to convert to quoted string
+  else
+    log_level=${1@U} # Bash-ish to convert to uppercase
+    message=${2} # Bash-ish to convert to quoted string
+  fi
+  
   if [[ ${log_level^^} == "INFO" ]]; then
     printf "%s [INFO] %s\n" "${timestamp}" "${message}"
   elif [[ ${log_level^^} == "ERROR" ]]; then
@@ -24,4 +34,29 @@ function log() {
     printf "%s [FATAL] %s is not a valid option.\nValid options for the log() function are: TRACE, DEBUG, INFO, WARN, ERROR\n" "${timestamp}" "${log_level}"
     return 1
   fi
+}
+
+# wrapper for log debug
+function log_debug(){
+  local log_message=$1
+
+  log "DEBUG" "${log_message}"
+}
+
+function log_info(){
+  local log_message=$1
+  
+  log "INFO" "${log_message}"
+}
+
+function log_warn(){
+  local log_message=$1
+
+  log "WARN" "${log_message}"
+}
+
+function log_error(){
+  local log_message=$1
+
+  log "ERROR" "${log_message}"
 }
